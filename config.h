b8cb731ebb07cd1c6ddda93e6066af7c0eb53b29
lxd12343 <3679435830@qq.com>
2026-08-29 19:36:24 +0800

    Fix MANUFACTURER/PRODUCT as string literals

--- config.h
+++ config.h
@@ -3,8 +3,8 @@
 #define VENDOR_ID 0xFEED
 #define PRODUCT_ID 0x0000
 #define DEVICE_VER 0x0001
-#define MANUFACTURER LXC
-#define PRODUCT lxc_kb
+#define MANUFACTURER "LXC"
+#define PRODUCT "lxc_kb"
 
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 17
