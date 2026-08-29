7dfc92ed0c9bb23e850c826990b825f16a89772a
lxd12343 <3679435830@qq.com>
2026-08-29 17:55:22 +0800

    Add configuration for matrix layout and debounce

---
+++ config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 17
+
+#define MATRIX_ROW_PINS { B0, B1, B2, B3, B4, B5, B6, B7 }
+#define MATRIX_COL_PINS { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A15, B8, B9, B10, B11, B12 }
+
+#define DIODE_DIRECTION COL2ROW
+
+#define DEBOUNCE 5
