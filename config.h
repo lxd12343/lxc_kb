fa5806121deb369afeb5e0dffef5db42c7e57f8e
lxd12343 <3679435830@qq.com>
2026-08-30 14:01:50 +0800

    Restore config.h from working version

--- config.h
+++ config.h
@@ -1,17 +1,17 @@
-#pragma once
+﻿#pragma once
 
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    "LXC"
-#define PRODUCT         "LXC-KB"
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0x0000
+#define DEVICE_VER 0x0001
+#define MANUFACTURER "LXC"
+#define PRODUCT "lxc_kb"
 
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 17
 
+#define MATRIX_ROW_PINS { B0, B1, B2, B3, B4, B5, B6, B7 }
+#define MATRIX_COL_PINS { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A15, B8, B9, B10, B11, B12 }
+
 #define DIODE_DIRECTION COL2ROW
 
 #define DEBOUNCE 5
-
-#define LOCKING_SUPPORT_ENABLE
-#define LOCKING_RESYNC_ENABLE
